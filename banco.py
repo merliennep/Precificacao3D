961edeb2096a5269cb6bfa4fbaf51b48ecad6552
Merlienne Pereira <merliennep@gmail.com>
2025-12-30 00:51:26 -0300

    alteração para banco de dados em nuvem

--- banco.py
+++ banco.py
@@ -6,26 +6,25 @@ def conectar():
         host=os.environ["DB_HOST"],
         database=os.environ["DB_NAME"],
         user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"]
+        password=os.environ["DB_PASSWORD"],
+        port=os.environ["DB_PORT"]
     )
-
 
 def criar_tabela():
     with conectar() as conn:
         cursor = conn.cursor()
         cursor.execute("""
-        CREATE TABLE pecas (
-        id SERIAL PRIMARY KEY,
-        nome TEXT,
-        material TEXT,
-        peso REAL,
-        tempo REAL,
-        custo_total REAL,
-        preco_venda REAL,
-        lucro REAL,
-        markup REAL,
-        data_criacao TIMESTAMP DEFAULT NOW()
-        );
-
+        CREATE TABLE IF NOT EXISTS pecas (
+            id SERIAL PRIMARY KEY,
+            nome TEXT,
+            material TEXT,
+            peso REAL,
+            tempo REAL,
+            custo_total REAL,
+            preco_venda REAL,
+            lucro REAL,
+            markup REAL,
+            data_criacao TIMESTAMP DEFAULT NOW()
+        )
         """)
         conn.commit()
